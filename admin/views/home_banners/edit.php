e9856d2a9ec74c1be3bc904a2210b0633d08af33
THANH TRUONG <23130356@st.hcmuaf.edu.vn>
2026-02-01 01:11:35 +0700

    sidebar admin

--- admin/views/home_banners/edit.php
+++ admin/views/home_banners/edit.php
@@ -18,7 +18,7 @@ if (!$banner) {
 $startDate = date('Y-m-d\TH:i', strtotime($banner['start_date']));
 $endDate = date('Y-m-d\TH:i', strtotime($banner['end_date']));
 ?>
-
+    
 <!DOCTYPE html>
 <html lang="vi">
 <head>
@@ -27,6 +27,7 @@ $endDate = date('Y-m-d\TH:i', strtotime($banner['end_date']));
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+    
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-8">
